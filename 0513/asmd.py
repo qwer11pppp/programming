67871a8bb6640e50caaadd0307f6c3ed51623a3c
qwer11pppp <65270303+qwer11pppp@users.noreply.github.com>
2020-05-13 15:39:05 +0900

    Add files via upload

---
+++ 0513/asmd.py
@@ -0,0 +1,17 @@
+a = int(input("첫 번째 숫자입력 : "))
+b = int(input("두 번째 숫자입력 : "))
+
+
+print(a,"+" , b, "=" ,(a+b))
+print(a,"-" , b, "=" ,(a-b))
+print(a,"*" , b, "=" ,(a*b))
+print(a,"/" , b, "=" ,(a/b))
+print(a,"//" , b, "=" ,(a//b))
+print(a,"%" , b, "=", (a%b))
+
+
+
+
+
+
+
