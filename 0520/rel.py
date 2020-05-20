a9d3d264ce2f55a4af9a53bb94c12016d4555c27
qwer11pppp <65270303+qwer11pppp@users.noreply.github.com>
2020-05-20 15:52:22 +0900

    Add files via upload

---
+++ 0520/rel.py
@@ -0,0 +1,10 @@
+#비교 연산자 : 두 수의 비교 결과를 boolean(True,False)
+a = int(input("첫 번쨰 수 입력 : "))
+b = int(input("두 번쨰 수 입력 : "))
+
+print(a, "==",b,(a==b)) #같다
+print(a, "!=",b,(a!=b)) #같지않다
+print(a, ">=",b,(a>=b)) #크거나같다
+print(a, "<=",b,(a<=b)) #작거나같다
+print(a, ">",b,(a>b)) #크다
+print(a, "<",b,(a<b)) #작다
